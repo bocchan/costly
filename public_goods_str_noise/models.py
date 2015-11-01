f72c5609b975d74ced70baec179c08c46ec7dbe4
Taiki Iida <track_alien@yahoo.co.jp>
2015-11-02 03:40:15 +0900

    noiseVerを作りました

---
+++ public_goods_str_noise/models.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+# <standard imports>
+from __future__ import division
+
+import random
+
+import otree.models
+import otree.constants
+from otree.db import models
+from otree import widgets
+from otree.common import Currency as c, currency_range, safe_json
+from otree.constants import BaseConstants
+from otree.models import BaseSubsession, BaseGroup, BasePlayer
+# </standard imports>
+
+author = 'Your name here'
+
+doc = """
+Your app description
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'public_goods_str_noise'
+    players_per_group = 3
+    num_rounds = 30
+
+    # define more constants here
+    endowment = c(20)
+    efficiency_factor = 0.5
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    # <built-in>
+    subsession = models.ForeignKey(Subsession)
+    # </built-in>
+    total_contribution = models.CurrencyField()
+    individual_share = models.CurrencyField()
+
+    def set_records(self):
+        for p in self.get_players():
+            x = random.random()
+            if x < 0.1:
+                p.record = 0
+            if x >= 0.1:
+                p.record = p.contribution
+
+    def set_payoffs(self):
+        self.total_contribution = sum([p.contribution for p in self.get_players()])
+        self.individual_share = self.total_contribution * Constants.efficiency_factor
+        for p in self.get_players():
+            p.income = Constants.endowment - p.contribution + self.individual_share
+            p.cost = p.punishment_p1 + p.punishment_p2 + p.punishment_p3
+            if p.id_in_group == 1:
+                p.deduction = sum([q.punishment_p1 for q in p.other_player()])
+            if p.id_in_group == 2:
+                p.deduction = sum([q.punishment_p2 for q in p.other_player()])
+            if p.id_in_group == 3:
+                p.deduction = sum([q.punishment_p3 for q in p.other_player()])
+            p.payoff = p.income - p.cost - 6 * p.deduction
+
+class Player(BasePlayer):
+    # <built-in>
+    subsession = models.ForeignKey(Subsession)
+    group = models.ForeignKey(Group, null = True)
+    # </built-in>
+
+    contribution = models.CurrencyField(choices=[0,Constants.endowment],widget=widgets.RadioSelect())
+    record = models.CurrencyField()
+    punishment_p1 = models.CurrencyField(min=0, max=50)
+    punishment_p2 = models.CurrencyField(min=0, max=50)
+    punishment_p3 = models.CurrencyField(min=0, max=50)
+    income = models.PositiveIntegerField(min=0, max=90)
+    cost  = models.PositiveIntegerField(min=0, max=150)
+    deduction = models.PositiveIntegerField(min=0, max=150)
+    def other_player(self):
+        """Returns other player in group. Only valid for 2-player groups."""
+        return self.get_others_in_group()
+
+    def set_payoff(self):
+        self.income = Constants.endowment - self.contribution + group.individual_share
+        self.cost = self.punishment_for_p1 + self.punishment_for_p2 + self.punishment_for_p3
+        self.deduction = sum([q.punishment_p1 for q in self.other_player()])
+
+    def role(self):
+        if self.id_in_group == 1:
+            return 'Player 1'
+        if self.id_in_group == 2:
+            return 'Player 2'
+        if self.id_in_group == 3:
+            return 'Player 3'
---
+++ public_goods_str_noise/views.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+
+from otree.common import Currency as c, currency_range, safe_json
+
+from . import models
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+
+class Contribute(Page):
+    form_model = models.Player
+    form_fields = ['contribution']
+
+class ResultsWaitPage(WaitPage):
+
+    def after_all_players_arrive(self):
+        self.group.set_records()
+
+
+class Punishment(Page):
+    form_model = models.Player
+    form_fields = ['punishment_p1', 'punishment_p2', 'punishment_p3']
+
+    
+class ResultsWaitPage2(WaitPage):
+
+    def after_all_players_arrive(self):
+        self.group.set_payoffs()
+
+
+class Results(Page):
+   pass
+
+
+
+page_sequence = [
+    Contribute,
+    ResultsWaitPage,
+    Punishment,
+    ResultsWaitPage2,
+    Results
+]
